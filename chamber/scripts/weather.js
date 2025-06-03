b7504ab59c622ec625c23f2c7b458854efb14fb8
divgray <dgiray@byupathway.edu>
2025-06-03 10:47:21 +0800

    again

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -98,3 +98,13 @@ document.addEventListener("DOMContentLoaded", () => {
     fetchCurrent();
     fetchForecast();
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const weatherIcon = document.getElementById('weather-icon');
+    const iconUrl = getWeatherIconUrl();
+    if (iconUrl) {
+        weatherIcon.src = iconUrl;
+    } else {
+        weatherIcon.src = "images/see.webp";
+    }
+});
