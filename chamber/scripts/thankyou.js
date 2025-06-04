a3e8755591d1d9d1df2d916ee105d3dd7cdd1b69
divgray <dgiray@byupathway.edu>
2025-06-04 14:15:20 +0800

    done dalan done .4

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,6 +1,11 @@
 const urlParams = new URLSearchParams(window.location.search);
-document.getElementById('display-first-name').textContent = urlParams.get('first-name') || '';
-document.getElementById('display-last-name').textContent = urlParams.get('last-name') || '';
+
+function capitalize(str) {
+    return str ? str.charAt(0).toUpperCase() + str.slice(1).toLowerCase() : '';
+}
+
+document.getElementById('display-first-name').textContent = capitalize(urlParams.get('first-name'));
+document.getElementById('display-last-name').textContent = capitalize(urlParams.get('last-name'));
 document.getElementById('display-email').textContent = urlParams.get('email') || '';
 document.getElementById('display-mobile').textContent = urlParams.get('mobile') || '';
 document.getElementById('display-business').textContent = urlParams.get('business-name') || '';
